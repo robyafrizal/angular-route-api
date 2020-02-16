ded508fdfb084a064282156f2e433506198957c4
robyafrizal <robyafrizal86@gmail.com>
2020-02-16 23:55:22 +0700

    test user page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { UsersComponent } from "./users/users.component";
 import { AlbumsComponent } from "./albums/albums.component";
 import { PhotosComponent } from "./photos/photos.component";
 
 const routes: Routes = [
   { path: "", component: AlbumsComponent },
+  { path: "users/", component: UsersComponent },
   { path: "photos/:albumId", component: PhotosComponent }
 ];
 
